288649ff600c6bc5dfe10df0fafc414cf097092c
Rahul6225 <rahul.thakurbk3000@gmail.com>
2024-11-24 21:22:16 +0530

    "Got to know about routing"

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
+app.get("/ab* c", (req, res) => {
+    res.send({ name: "Dev", lastName: "Simgh" });
+  });
+app.get("/ab+c", (req, res) => {
+    res.send({ name: "Dev", lastName: "Simgh" });
+  });
+  
+app.get("/use?r", (req, res) => {
+  res.send({ name: "rahul", lastName: "Simgh" });
+});
 
-app.use('/test',(req,res)=>{
-    res.send("Hello from Serevr");
-    
-})
+app.post("/user", (req, res) => {
+  res.send("data saved Successfully");
+});
+app.delete("/user", (req, res) => {
+  res.send("data deleted");
+});
+app.use("/test", (req, res) => {
+  res.send("Hello from Serevr");
+});
+app.listen(3000, () => {
+  console.log("Server Started on port:3000..");
+});
+// app.use('/hello',(req,res)=>{
+//     res.send("hello hello hello");  //Order of routing matters a lot
 
-app.listen(3000,()=>{
-    console.log("Server Started on port:3000..");
-})
+// })
+// app.use('/',(req,res)=>{
+//     res.send("Namastey Rahul")
+// })
